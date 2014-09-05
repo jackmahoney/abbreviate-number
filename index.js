3b18f6591b3e562b157b0b307e5ba5bdfa9dfaff
jackmahoney <jack@eyeem.com>
2014-09-05 16:57:26 +0200

    intl rm

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-var Intl = require('intl');
-
-function intlFormat(num)
-{
-    return new Intl.NumberFormat().format(Math.round(num*10)/10);
-}
-
 function abbr(num)
 {
 
     if(num >= 1000000){
-      	return intlFormat(num/1000000)+'m';    	
+      	return (num/1000000)+'m';    	
     }
     else if(num >= 1000){
-		return intlFormat(num/1000)+'k';
+		return (num/1000)+'k';
     }
     else{
-    	return intlFormat(num);	
+    	return (num);	
     }
    
 }
